a399bfecd7be1d0478c74e96b2d829cbbef62cb0
Mark Schweitzer <markdanielschweitzer@gmail.com>
2025-09-09 17:32:24 -0400

    current state of the tool frontend (ButtonTom.jsx) and the backend (Backend.py). Not ready to start direct testing but good to commit what I have so far.

---
+++ src/Pages/ButtonTom.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Container, Box, TextField, Button, Typography, Paper } from "@mui/material";
+
+export default function ButtonTom(){
+    {/* 
+    call = data.get('call', '')
+    primary = data.get('primary', '')
+    Secondary = data.get('secondary', '')
+    initlvl = data.get('initlvl', 0)
+    tolvl = data.get('tolvl', 0)
+    stat = data.get('stat', '')
+    curstat = data.get('curstat', 0) 
+    */}
+
+    const [prim, setPrim] = useState("");
+    const [seco, setSeco] = useState("");
+    const [initlvl, setInitLvl] = useState("");
+    const [tolvl, setToLvl] = useState("");
+    const [stat, setStat] = useState("");
+    const [curStat, setCurStat] = useState("");
+
+
+
+}
