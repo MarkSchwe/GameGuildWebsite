426c01601d0109bf8e1c08c78c988283c3bc3c26
Mark Schweitzer <markdanielschweitzer@gmail.com>
2025-09-16 15:04:47 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# The Official Website of The Periwinkle Squad Guild
-
-Made mostly by me, Mark Schweitzer
--- StartWebsite.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start cmd /k "cd C:\Users\budde\OneDrive\Documents\Workspace\PSquadWebsite && npm run dev"
-
-cd src/pages
-python Backend.py
