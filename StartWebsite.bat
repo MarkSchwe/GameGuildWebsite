dbc603cbbc2fc65273907a73d6336532142a3298
Mark Schweitzer <markdanielschweitzer@gmail.com>
2025-09-16 03:05:59 -0400

    Current versions for all files

---
+++ StartWebsite.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /k "cd C:\Users\budde\OneDrive\Documents\Workspace\PSquadWebsite && npm run dev"
+
+cd src/pages
+python Backend.py
--- src/Pages/ButtonTom.jsx
+++ src/Pages/ButtonTom.jsx
@@ -14,7 +14,7 @@ export default function ButtonTom(){
 
     const handleSubmit = async (e) => {
         try {
-            const res = await fetch("http://127.0.0.1:5000/process", {
+            const res = await fetch("/process", {
                 method: "POST",
                 headers: {"Content-Type":"application/json"},
                 body: JSON.stringify({
